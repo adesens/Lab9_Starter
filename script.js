230e6d379d8542d3844071c5f6d76d7d13789e3e
oladesens <aleksandra.desens@gmail.com>
2022-11-27 21:27:39 -0800

    Corrected typos

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function consoleWarn() {
 
 function consoleAssert() {
     console.assert(3 % 2 === 0, '3 is not an even number');
-    console.assert(2 % 2 === 0, '2 is an even number');
 }
 
 function consoleClear() {
@@ -39,20 +38,7 @@ function consoleGroupEnd() {
     console.groupEnd();
 }
 
-
-
 function consoleTable() {
-    
-    // function Dog(name, breed) {
-    //     this.name = name;
-    //     this.breed = breed;
-    // }
-
-    // const zoey = new Dog('Zoey', 'Goldendoodle');
-    // const abba = new Dog('Abba', 'Giant Schnauzer');
-    // const coco = new Dog('Coco', 'Shih Tzu');
-    // const charlie = new Dog('Charlie', 'French Bulldog');
-
     const zoey = {name: 'Zoey', breed: 'Goldendoodle'};
     const abba = {name: 'Abba', breed: 'Giant Schnauzer'};
     const coco = {name: 'Coco', breed: 'Shih Tzu'};
