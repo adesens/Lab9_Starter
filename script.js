cc05178611903928e4986ae11481f0ccc7da1f88
oladesens <aleksandra.desens@gmail.com>
2022-11-27 21:11:46 -0800

    Added console functions to script.js

--- script.js
+++ script.js
@@ -1,7 +1,78 @@
-function consoleLog(message) {
-    console.log(message);
+function consoleLog() {
+    console.log('Console Log Demo');
 }
 
-function consoleError(message) {
-    console.error(message);
+function consoleError() {
+    console.error('Console Error Demo');
+}
+
+function consoleCount() {
+    console.count('Console Count Button');
+}
+
+function consoleWarn() {
+    console.warn('Console Warn Demo');
+}
+
+function consoleAssert() {
+    console.assert(3 % 2 === 0, '3 is not an even number');
+    console.assert(2 % 2 === 0, '2 is an even number');
+}
+
+function consoleClear() {
+    console.clear();
+}
+
+function consoleDir() {
+
+}
+
+function consoleDirxml() {
+
+}
+
+function consoleGroupStart() {
+    console.group();
+}
+
+function consoleGroupEnd() {
+    console.groupEnd();
+}
+
+
+
+function consoleTable() {
+    
+    // function Dog(name, breed) {
+    //     this.name = name;
+    //     this.breed = breed;
+    // }
+
+    // const zoey = new Dog('Zoey', 'Goldendoodle');
+    // const abba = new Dog('Abba', 'Giant Schnauzer');
+    // const coco = new Dog('Coco', 'Shih Tzu');
+    // const charlie = new Dog('Charlie', 'French Bulldog');
+
+    const zoey = {name: 'Zoey', breed: 'Goldendoodle'};
+    const abba = {name: 'Abba', breed: 'Giant Schnauzer'};
+    const coco = {name: 'Coco', breed: 'Shih Tzu'};
+    const charlie = {name: 'Charlie', breed: 'French Bulldog'};
+
+    console.table([zoey, abba, coco, charlie]);
+}
+
+function startTimer() {
+    console.time();
+}
+
+function endTimer() {
+    console.timeEnd();
+}
+
+function consoleTrace() {
+
+}
+
+function triggerGlobalError() {
+
 }
